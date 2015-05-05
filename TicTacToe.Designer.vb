b080417020ad49b39507cdcde9902c3dea35a466
Pakyoli <pakyo_li@hotmail.com>
2015-05-05 17:46:26 -0400

    files
    files

---
+++ TicTacToe.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TicTacToe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.xLb = New System.Windows.Forms.Label()
+        Me.oLb = New System.Windows.Forms.Label()
+        Me.noLb = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.oCtr = New System.Windows.Forms.RadioButton()
+        Me.xCtr = New System.Windows.Forms.RadioButton()
+        Me.noStr = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'xLb
+        '
+        Me.xLb.AutoSize = True
+        Me.xLb.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.xLb.Location = New System.Drawing.Point(35, 33)
+        Me.xLb.Name = "xLb"
+        Me.xLb.Size = New System.Drawing.Size(52, 18)
+        Me.xLb.TabIndex = 0
+        Me.xLb.Text = "X wins"
+        '
+        'oLb
+        '
+        Me.oLb.AutoSize = True
+        Me.oLb.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oLb.Location = New System.Drawing.Point(35, 74)
+        Me.oLb.Name = "oLb"
+        Me.oLb.Size = New System.Drawing.Size(54, 18)
+        Me.oLb.TabIndex = 1
+        Me.oLb.Text = "O wins"
+        '
+        'noLb
+        '
+        Me.noLb.AutoSize = True
+        Me.noLb.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.noLb.Location = New System.Drawing.Point(35, 118)
+        Me.noLb.Name = "noLb"
+        Me.noLb.Size = New System.Drawing.Size(62, 18)
+        Me.noLb.TabIndex = 2
+        Me.noLb.Text = "No wins"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.xLb)
+        Me.GroupBox1.Controls.Add(Me.noLb)
+        Me.GroupBox1.Controls.Add(Me.oLb)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(267, 74)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(164, 160)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Result"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.AutoSize = True
+        Me.GroupBox2.Controls.Add(Me.Panel1)
+        Me.GroupBox2.Controls.Add(Me.oCtr)
+        Me.GroupBox2.Controls.Add(Me.xCtr)
+        Me.GroupBox2.Controls.Add(Me.noStr)
+        Me.GroupBox2.Location = New System.Drawing.Point(30, 271)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(401, 169)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Strategy"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(23, 64)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(341, 86)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoEllipsis = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(21, 11)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(305, 75)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "xxxx"
+        '
+        'oCtr
+        '
+        Me.oCtr.AutoSize = True
+        Me.oCtr.Location = New System.Drawing.Point(290, 32)
+        Me.oCtr.Name = "oCtr"
+        Me.oCtr.Size = New System.Drawing.Size(74, 17)
+        Me.oCtr.TabIndex = 2
+        Me.oCtr.TabStop = True
+        Me.oCtr.Text = "Strategy C"
+        Me.oCtr.UseVisualStyleBackColor = True
+        '
+        'xCtr
+        '
+        Me.xCtr.AutoSize = True
+        Me.xCtr.Location = New System.Drawing.Point(152, 32)
+        Me.xCtr.Name = "xCtr"
+        Me.xCtr.Size = New System.Drawing.Size(74, 17)
+        Me.xCtr.TabIndex = 1
+        Me.xCtr.TabStop = True
+        Me.xCtr.Text = "Strategy B"
+        Me.xCtr.UseVisualStyleBackColor = True
+        '
+        'noStr
+        '
+        Me.noStr.AutoSize = True
+        Me.noStr.Location = New System.Drawing.Point(23, 32)
+        Me.noStr.Name = "noStr"
+        Me.noStr.Size = New System.Drawing.Size(74, 17)
+        Me.noStr.TabIndex = 0
+        Me.noStr.TabStop = True
+        Me.noStr.Text = "Strategy A"
+        Me.noStr.UseVisualStyleBackColor = True
+        '
+        'TicTacToe
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(443, 462)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "TicTacToe"
+        Me.Text = "Tic-Tac-Toe"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents xLb As System.Windows.Forms.Label
+    Friend WithEvents oLb As System.Windows.Forms.Label
+    Friend WithEvents noLb As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents oCtr As System.Windows.Forms.RadioButton
+    Friend WithEvents xCtr As System.Windows.Forms.RadioButton
+    Friend WithEvents noStr As System.Windows.Forms.RadioButton
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ TicTacToe.vb
@@ -0,0 +1,258 @@
+﻿Public Class TicTacToe
+    Friend WithEvents myButton As System.Windows.Forms.Button
+    Dim board(3, 3) As Button
+    Dim r As New Random(1000)
+    Dim symbol(2) As Char
+    Dim player As Char
+    Dim foundWin As Boolean = False
+    Dim xWin As Integer = 0
+    Dim oWin As Integer = 0
+    Dim noWin As Integer = 0
+    Dim numrow, numcol As Integer
+    Dim counter As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' create the board
+        myButton = New System.Windows.Forms.Button()
+
+        Dim num As Integer
+
+        For row = 1 To 3
+            For col = 1 To 3
+                myButton = New System.Windows.Forms.Button()
+                With myButton
+                    num = r.Next(1, 3)
+                    .Text = ""
+                    .Location = New System.Drawing.Point(col * 60, row * 60)
+                    .Size = New System.Drawing.Size(60, 60)
+                End With
+                Me.Controls.Add(myButton)
+                board(row, col) = myButton
+            Next col
+        Next row
+    End Sub
+    '
+    'default strategy
+    'random plays
+    Private Sub strategyA()
+
+        While counter < 1000
+            'clear the board at the beginning of every run
+            clearBoard()
+            'initialize player at the beginning of every run
+            initializePlayer()
+
+            For i = 1 To 9
+                Do While True
+                    numrow = r.Next(1, 4)
+                    numcol = r.Next(1, 4)
+                    If board(numrow, numcol).Text = "" Then
+                        board(numrow, numcol).Text = player
+                        If (i >= 5) Then
+                            'after 5 moves, check if someone wins the game
+                            If CheckWin(player) Then
+                                foundWin = True
+                                countWins()
+                                'end the current game when winner is found
+                                Exit For
+                            End If
+                        End If
+                        'call changeTurn sub procedure to do the turn change
+                        changeTurn()
+                        Exit Do
+                    End If
+                Loop
+            Next
+
+            If foundWin = False Then
+                noWin += 1
+            End If
+            'counter the numbers of games ran
+            counter += 1
+            'reset the found winner var to false
+            foundWin = False
+        End While
+        'call printResult sub procedure to print the result
+        printResult()
+        'call resetCount sub procedure to reset all the counters for the next strategy
+        resetCount()
+    End Sub
+
+    Private Sub strategyB()
+
+        While counter < 1000
+            clearBoard()
+            initializePlayer()
+            '
+            'Have X move in the center spac
+            board(2, 2).Text = player
+            player = symbol(2)
+
+            For i = 2 To 9
+                Do While True
+                    numrow = r.Next(1, 4)
+                    numcol = r.Next(1, 4)
+                    If board(numrow, numcol).Text = "" Then
+                        board(numrow, numcol).Text = player
+                        If (i >= 5) Then
+
+                            If CheckWin(player) Then
+                                foundWin = True
+                                countWins()
+                                Exit For
+                            End If
+                        End If
+                        changeTurn()
+                        Exit Do
+                    End If
+                Loop
+            Next
+
+            If foundWin = False Then
+                noWin += 1
+            End If
+
+            counter += 1
+            foundWin = False
+        End While
+
+        printResult()
+        resetCount()
+    End Sub
+
+    Private Sub strategyC()
+
+        While counter < 1000
+            clearBoard()
+            initializePlayer()
+
+            For i = 1 To 9
+                Do While True
+                    numrow = r.Next(1, 4)
+                    numcol = r.Next(1, 4)
+
+                    Do While i = 2
+                        'have the O move in the center if X didn't on the first turn
+                        If board(2, 2).Text = "" Then
+                            board(2, 2).Text = player
+                            'if O move into the center, change turn
+                            GoTo line1
+                        Else
+                            'else, randomly placement
+                            GoTo line2
+                        End If
+                    Loop
+line2:
+                    If board(numrow, numcol).Text = "" Then
+                        board(numrow, numcol).Text = player
+
+                        If (i >= 5) Then
+
+                            If CheckWin(player) Then
+                                foundWin = True
+                                countWins()
+                                Exit For
+                            End If
+                        End If
+line1:
+                        changeTurn()
+                        Exit Do
+                    End If
+                Loop
+            Next
+
+            If foundWin = False Then
+                noWin += 1
+            End If
+
+            counter += 1
+            foundWin = False
+        End While
+
+        printResult()
+        resetCount()
+    End Sub
+    Private Function CheckWin(ByVal inplayer As String) As Boolean
+
+        If board(1, 1).Text = inplayer And board(2, 1).Text = inplayer And board(3, 1).Text = inplayer Then
+            Return True
+        ElseIf board(1, 1).Text = inplayer And board(1, 2).Text = inplayer And board(1, 3).Text = inplayer Then
+            Return True
+        ElseIf board(1, 1).Text = inplayer And board(2, 2).Text = inplayer And board(3, 3).Text = inplayer Then
+            Return True
+        ElseIf board(1, 3).Text = inplayer And board(2, 2).Text = inplayer And board(3, 1).Text = inplayer Then
+            Return True
+        ElseIf board(2, 1).Text = inplayer And board(2, 2).Text = inplayer And board(2, 3).Text = inplayer Then
+            Return True
+        ElseIf board(3, 1).Text = inplayer And board(3, 2).Text = inplayer And board(3, 3).Text = inplayer Then
+            Return True
+        ElseIf board(1, 2).Text = inplayer And board(2, 2).Text = inplayer And board(3, 2).Text = inplayer Then
+            Return True
+        ElseIf board(1, 3).Text = inplayer And board(2, 3).Text = inplayer And board(3, 3).Text = inplayer Then
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Private Sub noStr_CheckedChanged(sender As Object, e As EventArgs) Handles noStr.CheckedChanged
+        'print the strategy
+        Label4.Text = "No strategy, random placement."
+        strategyA()
+    End Sub
+
+    Private Sub xCtr_CheckedChanged(sender As Object, e As EventArgs) Handles xCtr.CheckedChanged
+        Label4.Text = "Have X move in the center space for every game."
+        strategyB()
+    End Sub
+
+    Private Sub oCtr_CheckedChanged(sender As Object, e As EventArgs) Handles oCtr.CheckedChanged
+        Label4.Text = "Have O move in the center space first, for games where X does not randomly move there first."
+        strategyC()
+    End Sub
+
+    Private Sub initializePlayer()
+        symbol(1) = "X"
+        symbol(2) = "O"
+        player = symbol(1)
+    End Sub
+    Private Sub countWins()
+        If player = symbol(1) Then
+            xWin += 1
+        ElseIf player = symbol(2) Then
+            oWin += 1
+        End If
+    End Sub
+
+    Private Sub changeTurn()
+        If player = symbol(1) Then
+            player = symbol(2)
+        Else
+            player = symbol(1)
+        End If
+    End Sub
+
+    Private Sub clearBoard()
+        For row = 1 To 3
+            For col = 1 To 3
+                board(row, col).Text = ""
+            Next
+        Next
+    End Sub
+
+    Private Sub printResult()
+        xLb.Text = "X wins " + xWin.ToString
+        oLb.Text = "O wins " + oWin.ToString
+        noLb.Text = "No wins " + noWin.ToString
+    End Sub
+
+    Private Sub resetCount()
+        xWin = 0
+        oWin = 0
+        noWin = 0
+        foundWin = False
+        counter = 0
+    End Sub
+
+End Class
+
